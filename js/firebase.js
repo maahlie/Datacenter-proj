f95412cb6b039a463fdf26795083680ba4fa44ce
ahmedfowzi <Ahmed.Fowzi.Tabit@Student.GildeOpleidingen.nl>
2023-03-13 14:22:03 +0100

    reed contact

--- js/firebase.js
+++ js/firebase.js
@@ -25,6 +25,10 @@ function GetLastRecord() {
             document.getElementById("cpu_usage").innerHTML = element.val().cpu_usage;
             document.getElementById("ram_usage").innerHTML = element.val().ram_usage;
             document.getElementById("pre-loader").style.display = "none";
+            var serverkast = (element.val().serverkast == 0) ? ["status status_danger", "fa-solid fa-door-open", "Open"] : ["status status_success", "fa-solid fa-door-closed", "Dicht"];
+            document.getElementById("serverkast_status").className = serverkast[0];
+            document.getElementById("serverkast_icon").className = serverkast[1];
+            document.getElementById("serverkast").innerHTML = serverkast[2];
         });
 
     });
